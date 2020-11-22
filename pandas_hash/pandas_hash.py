24ea9a9009760177ad6569f4e3f1162edcb96991
Alan Bertl <alan.c.bertl@gmail.com>
2020-11-22 00:23:52 -0600

    More consistent way of hashing pandas dataframes

--- pandas_hash/pandas_hash.py
+++ pandas_hash/pandas_hash.py
@@ -2,30 +2,36 @@ import pandas as pd
 from hashlib import sha256
 from pandas.util import hash_pandas_object
 
+class PandasWrapper:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def __hash__(self):
+        return hash((
+            int(sha256(hash_pandas_object(self.obj).values).hexdigest(), 16),
+            hash(self.obj.index.name),
+            hash(self.obj.columns.name)
+        ))
+
+
 def convert_hashable(obj):
     try:
         hash(obj)
     except TypeError:
         if type(obj) is list:
-            return tuple(convert_hashable(i) for i in obj)
+            return (list, tuple(convert_hashable(i) for i in obj))
         elif type(obj) is set:
-            return frozenset(convert_hashable(i) for i in obj)
+            return (set, frozenset(convert_hashable(i) for i in obj))
         elif type(obj) is dict:
-            return frozenset((convert_hashable(k), convert_hashable(v)) for k,v in obj.items())
-        elif hasattr(obj, '__dict__'):
-            return convert_hashable(obj.__dict__)
+            return (dict, frozenset((convert_hashable(k), convert_hashable(v)) for k,v in obj.items()))
+        elif isinstance(obj, pd.DataFrame):
+            return PandasWrapper(obj)
+        else:
+            raise TypeError(f"Don't know how to convert object of type {type(obj)} to hashable")
+        # elif hasattr(obj, '__dict__'):
+            # return convert_hashable(obj.__dict__)
     else:
         return obj
 
 def hash_object(obj):
-    try:
-        return hash(obj)
-    except TypeError:
-        if isinstance(obj, pd.DataFrame):
-            return (
-                sha256(hash_pandas_object(obj).values).hexdigest() + 
-                hex(hash(obj.index.name))[2:] + 
-                hex(hash(obj.columns.name))[2:]
-            )
-        else:
-            return hash(type(obj)) ^ hash(convert_hashable(obj))
+        return hash(convert_hashable(obj))
