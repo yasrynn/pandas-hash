12aaa1ec7013ea3c0eedce1deef2eeea26c0aa10
Alan Bertl <alan.c.bertl@gmail.com>
2020-12-23 00:59:35 -0600

    Changed wrapper to handle both dataframe and series

--- pandas_hash/pandas_hash.py
+++ pandas_hash/pandas_hash.py
@@ -1,15 +1,18 @@
 import pandas as pd
 from pandas.util import hash_pandas_object
 
-class PandasDataFrameWrapper:
+class PandasObjectWrapper:
     def __init__(self, obj):
         self.obj = obj
+        name = obj.name if hasattr(obj, 'name') else None
+        index_name = obj.index.name if hasattr(obj, 'index') else None
+        columns_name = obj.columns.name if hasattr(obj, 'columns') else None
+        self.index_names = (name, index_name, columns_name)
 
     def __hash__(self):
         return hash((
             tuple(hash_pandas_object(self.obj).tolist()),
-            hash(self.obj.index.name),
-            hash(self.obj.columns.name)
+            hash(self.index_names),
         ))
 
 
@@ -23,8 +26,8 @@ def convert_hashable(obj):
             out_obj = frozenset(convert_hashable(i) for i in obj)
         elif type(obj) is dict:
             out_obj = frozenset((k, convert_hashable(v)) for k,v in obj.items())
-        elif isinstance(obj, pd.DataFrame):
-            out_obj = PandasDataFrameWrapper(obj)
+        elif isinstance(obj, pd.Series) or isinstance(obj, pd.DataFrame):
+            out_obj = PandasObjectWrapper(obj)
         else:
             raise TypeError(f"Don't know how to convert object of type {type(obj)} to hashable")
         # elif hasattr(obj, '__dict__'):
